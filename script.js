9e56b77821ba4ea0b7f5fab07d0265aa117e2109
hafizwebuilder <hafizullah.sarwary1024@gmail.com>
2026-04-28 00:02:45 +0200

    Redesign Tehran Lavaggio website with SEO-focused multi-page structure

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.main-nav');
+if (toggle && nav) {
+  toggle.addEventListener('click', () => {
+    const isOpen = nav.classList.toggle('open');
+    toggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
+  });
+}
